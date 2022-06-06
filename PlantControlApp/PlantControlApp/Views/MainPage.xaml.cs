bd1aed134b2b5abd9df9dd0d3a61fabd32bff8cd
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2022-06-06 14:28:37 +0200

    Navigation between pages + App Icon

--- PlantControlApp/PlantControlApp/Views/MainPage.xaml.cs
+++ PlantControlApp/PlantControlApp/Views/MainPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using PlantControlApp.Models;
 
 namespace PlantControlApp.Views
 {
@@ -13,6 +9,18 @@ namespace PlantControlApp.Views
         public MainPage()
         {
             InitializeComponent();
+            flyoutPage.listView.ItemSelected += OnItemSelected;
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as FlyoutPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance((Type)item.TargetType));
+                flyoutPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
