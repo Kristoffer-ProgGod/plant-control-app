3466130329a22070f03e32f0865876b9205252f7
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2022-06-06 19:16:03 +0200

    PlantView List + start of create plant

---
+++ PlantControlApp/PlantControlApp/ViewModels/PlantViewModel.cs
@@ -0,0 +1,33 @@
+﻿using PlantControl.Xamarin.Models;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace PlantControlApp.ViewModels
+{
+    internal class PlantViewModel
+    {
+        private List<Plant> plants;
+
+        public List<Plant> Plants
+        {
+            get { return plants; }
+            set { plants = value; }
+        }
+
+        private ICommand switchView;
+
+        public ICommand SwitchView
+        {
+            get { return switchView; }
+            set { switchView = value; }
+        }
+
+        public void CreatePlant()
+        {
+
+        }
+
+
+
+    }
+}
