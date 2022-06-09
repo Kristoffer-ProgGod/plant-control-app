0f46721df4b975ecd83c9c0a80a8888d739c98ff
CKDenni <deni3278@easv365.dk>
2022-06-09 16:15:47 +0200

    Change PairingView

---
+++ PlantControlApp/PlantControlApp/Services/ScannerService.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+using ZXing.Mobile;
+
+namespace PlantControlApp.Services;
+
+public class ScannerService
+{
+    private readonly MobileBarcodeScanner _scanner;
+    
+    public ScannerService()
+    {
+        _scanner = new MobileBarcodeScanner();
+    }
+
+    public async Task<string> Scan(string topText = "", string bottomText = "")
+    {
+        await Permissions.RequestAsync<Permissions.Camera>();
+
+        _scanner.TopText = topText;
+        _scanner.BottomText = bottomText;
+
+        return (await _scanner.Scan()).Text;
+    }
+}
