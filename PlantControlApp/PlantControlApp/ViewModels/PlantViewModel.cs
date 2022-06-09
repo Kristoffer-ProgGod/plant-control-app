762be73c8312db61c4afdfb952f95e29772f74df
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2022-06-09 16:21:16 +0200

    All the get plant shit

--- PlantControlApp/PlantControlApp/ViewModels/PlantViewModel.cs
+++ PlantControlApp/PlantControlApp/ViewModels/PlantViewModel.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Windows.Input;
 using PlantControl.Models;
 using Xamarin.Forms;
-
 namespace PlantControlApp.ViewModels;
 
 internal class PlantViewModel : Bindable
@@ -18,4 +18,7 @@ internal class PlantViewModel : Bindable
     public List<Plant> Plants { get; set; }
 
     public ICommand SwitchViewCommand { get; set; }
+    private void GetPlants()
+    {
+    }
 }
