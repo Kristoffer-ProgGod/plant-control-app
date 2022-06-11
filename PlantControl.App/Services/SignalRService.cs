b8f3a55478387a7e69dcc81721d6e469be754628
Rasmus <66802136+rasmus234@users.noreply.github.com>
2022-06-11 22:41:12 +0200

    fix incorrect merge

--- PlantControl.App/Services/SignalRService.cs
+++ PlantControl.App/Services/SignalRService.cs
@@ -12,9 +12,9 @@ public class SignalRService
     public SignalRService()
     {
         Connection = new HubConnectionBuilder()
-            .WithUrl("https://plant-control-backend.herokuapp.com/hubs/logger")
+            // .WithUrl("https://plant-control-backend.herokuapp.com/hubs/logger")
             // .WithUrl("http://10.0.2.2:5140/hubs/logger")
-            // .WithUrl("http://40.87.132.220:9093/hubs/logger")
+            .WithUrl("http://40.87.132.220:9093/hubs/logger")
             .ConfigureLogging(builder => builder.AddDebug())
             .Build();
         Connection.On<Config>("ReceiveConfig", config => OnReceiveConfig?.Invoke(config));
